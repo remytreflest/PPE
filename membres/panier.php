015a677528f644c8c14fe1d2aabcbf08a0dc359b
treflest remy <mrtreflestremy@outlook.com>
2021-04-15 14:19:17 +0200

    pour mettre à jour

--- membres/panier.php
+++ membres/panier.php
@@ -8,18 +8,24 @@ require_once "header.php";
             <div class="card">
                 <div class="card-body">
                     <card class="card-text">
-                        <div class="py-2">MON PANIER</div>
+                        <div class="py-2 bold">MON PANIER</div>
                     </card>
                     <hr class="dropdown-divider">
                     <card class="card-text d-flex row">
-                        <div class="col-12 col-lg-3">
+                        <div class="col-12 col-lg-4">
                             <div id="image-produit-panier"></div>
                         </div>
-                        <div class="card-body col-12 col-lg-9 d-flex flex-column">
-                            <div class="card-text">Prix</div>
-                            <div class="card-text">Description du voyage</div>
-                            <div class="card-text d-flex align-items-center">
-                                <div>Qté : 1 </div>
+                        <div class="card-body col-12 col-lg-8 d-flex flex-column">
+                            <div class="card-text text-gris my-2">Prix</div>
+                            <div class="card-text text-gris my-2">Nom du voyage</div>
+                            <div class="card-text my-2">
+                                <div class="text-gris">Récapitulatif :</div>
+                                <div class="ml-3">- Voyage de A vers B sur 2 jours</div>
+                                <div class="ml-3">- Voyage de B vers C sur 2 jours</div>
+                                <div class="ml-3">- Voyage de C vers D sur 2 jours</div>
+                            </div>
+                            <div class="card-text my-2 d-flex align-items-center">
+                                <div class="text-gris">Nombre de voyageur(s) : 1 </div>
                                 <button class="panier-quantite ml-2">-</button>
                                 <button class="panier-quantite ml-1">+</button>
                             </div>
@@ -29,7 +35,10 @@ require_once "header.php";
                     </card>
                     <hr class="dropdown-divider">
                     <card class="card-text">
-                        <div class="py-2">PAIEMENT</div>
+                        <div class="d-flex justify-content-end py-2">
+                            <div class="bold">TOTAL :</div>
+                            <div class="bold ml-4">XX,XX €</div>
+                        </div>
                     </card>
                 </div>
             </div>
@@ -37,10 +46,10 @@ require_once "header.php";
         <div id="card-paiement" class="col-12 col-lg-4 pl-lg-0">
             <div class="card">
                 <div class="card-body">
-                    <div class="card-text py-2">TOTAL</div>
+                    <div class="card-text bold py-2">TOTAL</div>
                     <div class="card-text py-2">
                         <div class="d-flex justify-content-between">
-                            <div>SOUS TOTAL</div>
+                            <div>VOYAGE 1</div>
                             <div>xx.xx €</div>
                         </div>
                         <div class="d-flex justify-content-between">
